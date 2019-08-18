1fc2a7d1e857c204cbf7fa3678c1f139e6dba8f3
ftech-ai <mrnobody@ftech-ai>
2019-08-18 07:44:43 +0700

    - Init

---
+++ Example/RocketChatViewController Example/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  RocketChatViewController Example
+//
+//  Created by Rafael Kellermann Streit on 30/07/18.
+//  Copyright © 2018 Rocket.Chat. All rights reserved.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    
+
+}
+
